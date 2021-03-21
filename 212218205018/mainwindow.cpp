15392d00cbeea881953052242a1d92e254dd273f
Janakiram Jonnalagadda <53149799+janakiramjonnalagadda@users.noreply.github.com>
2021-03-21 16:23:48 +0530

    Add files via upload

---
+++ 212218205018/mainwindow.cpp
@@ -0,0 +1,58 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QApplication>
+#include <QDebug>
+#include <QRegularExpression>
+#include <QValidator>
+#include <QRegularExpressionMatch>
+#include <QRegularExpressionValidator>
+#include<QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QMessageBox::StandardButton reply=QMessageBox::information(this,"Janakiram-212218205018","Submitted");
+
+}
+
+void MainWindow::on_lineEdit_returnPressed()
+{
+    if(ui->lineEdit->text().length()>200)
+        QMessageBox::information(this,"Janakiram-212218205018","More than 50 words in Area of Interest");
+
+}
+
+void MainWindow::on_lineEdit_2_returnPressed()
+{
+    QRegularExpression sh("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$");
+    QRegularExpressionMatch match = sh.match(ui->lineEdit_2->text());
+    bool hasMatch=match.hasMatch();
+    if(!hasMatch)
+    {
+       QMessageBox::information(this,"Janakiram- 212218205018","EmailID is invalid");
+    }
+
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    foreach(QLineEdit* widget, findChildren<QLineEdit*>())
+    {
+        widget->clear();
+    }
+}
